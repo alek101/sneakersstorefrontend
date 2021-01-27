f4a65c273d86309140317a04750c99598e8ed013
alek101 <47416711+alek101@users.noreply.github.com>
2021-01-27 19:27:21 +0100

    save

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
+import {useState, useEffect} from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Header from './components/Header';
 import Home from './components/Home';
 import Purchase from './components/Purchase';
 import Past from './components/Past';
-
 import {createStore} from 'redux';
 import allReducers from './reducers';
 import {Provider} from 'react-redux';
@@ -12,8 +12,31 @@ const store = createStore(
   allReducers,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
-
 function App() {
+
+  const [data, setData] = useState(null);
+  // const [error, setError] =useState(null);
+  const url='http://127.0.0.1:8000/api/products';
+
+  useEffect(()=>{
+      const abortController=new AbortController();
+      fetch(url, {signal: abortController.signal})
+      .then(res=>{
+          if(!res.ok){
+              throw Error(`Couldn't fetch the data`);
+          }
+          return res.json()
+      })
+      .then(res=>{
+      //   console.log(res);
+        setData(res);  
+      })
+      .catch(err=>console.log(err));
+
+      // return () =>abortController.abort();
+  },[])
+
+
   return (
     <Provider store={store}>
       <Router>
@@ -22,7 +45,7 @@ function App() {
           <div className="content">
             <Switch>
               <Route exact path="/">
-                <Home />
+                <Home data={data}/>
               </Route>
               <Route path="/purchase">
                 <Purchase />
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,29 +1,29 @@
-import {useState, useEffect} from 'react';
+// import {useState, useEffect} from 'react';
 import Product from './Product';
 
-const Home = () => {
+const Home = ({data}) => {
 
-    const [data, setData] = useState(null);
-    // const [error, setError] =useState(null);
-    const url='http://127.0.0.1:8000/api/products';
+    // const [data, setData] = useState(null);
+    // // const [error, setError] =useState(null);
+    // const url='http://127.0.0.1:8000/api/products';
 
-    useEffect(()=>{
-        const abortController=new AbortController();
-        fetch(url, {signal: abortController.signal})
-        .then(res=>{
-            if(!res.ok){
-                throw Error(`Couldn't fetch the data`);
-            }
-            return res.json()
-        })
-        .then(res=>{
-        //   console.log(res);
-          setData(res);  
-        })
-        .catch(err=>console.log(err));
+    // useEffect(()=>{
+    //     const abortController=new AbortController();
+    //     fetch(url, {signal: abortController.signal})
+    //     .then(res=>{
+    //         if(!res.ok){
+    //             throw Error(`Couldn't fetch the data`);
+    //         }
+    //         return res.json()
+    //     })
+    //     .then(res=>{
+    //     //   console.log(res);
+    //       setData(res);  
+    //     })
+    //     .catch(err=>console.log(err));
 
-        // return () =>abortController.abort();
-    },[])
+    //     // return () =>abortController.abort();
+    // },[])
 
     const products=(data)? data.map(product=><Product key={product.id} product={product} />):<div className="loading">Loading...</div>;
 
